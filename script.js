d7b15be2339027f59593ce196132c58873167703
Santiago <sctunal@gmail.com>
2022-06-18 22:29:09 -0500

    Change strcture to create instances and add the function of Clean Data

--- script.js
+++ script.js
@@ -7,35 +7,6 @@ const indexedDB = window.indexedDB;
  * TODOS: Si no terminar usuarios
  * TODOS: Si no terminar DATA
 */
-var idInputUser = [
-    'idUser',
-    'nameUser',
-    'passwordUser',
-    'emailUser',
-    'roleUser',
-];
-var idInputScientist = [
-    'jobScientist',
-    'experienceScientist',
-    'backgroundScientist',
-    "styleScientist"
-];
-var idInputData = [
-    'nameData',
-    'fileData',
-    'typeData',
-    "classificationData",
-    "descriptionData",
-    "formatData",
-]
-var idInputDataAuto =[
-    'version',
-    'creation',
-    'update',
-    'size',
-    'quality',
-];
-var size;
 if(indexedDB){
     let db;
     const request = indexedDB.open('betaDB',1) //El segundo numero es la version del a bd
@@ -79,6 +50,26 @@ if(indexedDB){
         }
 
     }
+    function updateData(store, data){
+        let key = data['name']
+        const transaction = db.transaction([store], 'readwrite');
+        const objectStore = transaction.objectStore(store);
+
+        const requestGet = objectStore.get(key);
+        requestGet.onsuccess = (ev) => {
+            data['creation'] = ev.target.result.creation;
+            data['version'] = ev.target.result.version + 1;
+            const requestPut = objectStore.put(data)
+            requestPut.onsuccess = function(e){
+                console.log('exit')
+                window.location.href='../index.html'
+            }
+            request.onerror = function(e){
+                window.alert(e.target.error.message);
+            }
+            }
+
+    }
     function fillUsers(store,idBody){
         const transaction = db.transaction([store]);
         const objectStore = transaction.objectStore(store);
@@ -90,7 +81,6 @@ if(indexedDB){
                 $('#'+idBody).append(
                     '<tr><td>' + cursor.value.identification + 
                     '</td><td>' + cursor.value.name + 
-                    '</td><td>' + cursor.value.password + 
                     '</td><td>' + cursor.value.email + 
                     '</td><td>' + cursor.value.role + '</td></tr>'
                 )
@@ -184,71 +174,61 @@ if(indexedDB){
             cursor.continue();
         }
     }
-    // function size(store,action){
-    //     const transaction = db.transaction([store]);
-    //     const objectStore = transaction.objectStore(store);
-    //     objectStore.getAll().onsuccess = function(event){
-    //         let request = event.target;
-    //         // instances =  request.result.length;
-    //     }
-
-    // }
-    function formData(action){
-        var formValues = {};
-        if(action == 'createUser'){
-            idInputUser.forEach(function(id){
-                let name = $("#"+id).attr('name');
-                let value = $("#"+id).val();
-                if(name == "password"){
-                    value = CryptoJS.MD5(value);
-                }
-                formValues[name]=value;
-            })
-            let role = $("#"+idInputUser[idInputUser.length - 1]).val();
-            if(role=="DATA SCIENTIST"){
-                idInputScientist.forEach(function(id){
-                    let name = $("#"+id).attr('name');
-                    let value = $("#"+id).val();
-                    formValues[name]=value;
-                });
-            }
-            addData('users',formValues);
-        }else if(action == 'createData'){
-            console.log('entro');
-            idInputData.forEach(function(id){
-                let name = $('#'+id).attr('name');
-                let value = $('#'+id).val();
-                formValues[name]=value;
-            });
-            idInputDataAuto.forEach(function(name){
-                if(name=='version'){
-                    formValues[name] = '1';
-                }else if(name=='creation'){
-                    let today = new Date();
-                    let creation = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-                    formValues[name] = creation;
-                }else if(name=='update'){
-                    let today = new Date();
-                    let update = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-                    formValues[name] = update;
-                }else if(name=='quality'){
-                    formValues[name] ='Low';
-                }else if(name=='size'){
-                    formValues[name] = size;
-                }
-            })
-            addData('data',formValues);
-        }
+    loadProvideClean = () =>{
+        loadProvideData();
+        var QueryId = (new URL(location.href)).searchParams.get('id');
+        console.log(QueryId);
+        const transaction = db.transaction(['data']);
+        const objectStore = transaction.objectStore('data');
+        const request = objectStore.get(QueryId);
+        request.onsuccess = (ev) =>{
+            let data  = ev.target.result;
+            console.log(data['name']);
+            console.log(data['type']);
+            $('#nameDataClean').val(data['name']);
+            // $('#nameDataClean').attr('disabled','disabled');
+            $('#typeData').val(data['type']).prop('selected',true).trigger('change');
+            $('#classificationData').val(data['classification']);
+            $('#descriptionData').val(data['description']);
+            $('#formatData').val(data['format']);
+        } 
     }
 
-
 }
-// var size = $('#fileData').on('change',function(){
-//     size = this.files[0].size;
-// })
-function fileSize(id){
-    size = $('#'+id)[0].files[0].size;
-    size = Math.round(size/1024) + 'KB';
+saveUser = (form, event) =>{
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(form).entries());
+    data = Object.fromEntries(Object.entries(data).filter(([_,v])=> v != ''))
+    addData('users',data);
+}
+saveData = (form, event)=>{
+    event.preventDefault();
+    let data = Object.fromEntries(new FormData(form).entries());
+    const today = new Date();
+    const creation = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ':' + today.getMinutes() + ':' +today.getSeconds();
+    data = Object.fromEntries(Object.entries(data).filter(([_,v])=> v != ''))
+    const size = Math.round(data['file'].size/1024) + 'KB';
+    data['file'] = data['file'].name;
+    data['size'] = size;
+    data['creation'] = creation;
+    data['update'] = creation;
+    data['quality'] = 'Low';
+    data['version'] = 1;
+    addData('data',data);
+}
+saveDataClean = (form, event)=>{
+    event.preventDefault();
+    let dataUpdate = Object.fromEntries(new FormData(form).entries());
+    const today = new Date();
+    const update = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ':' + today.getMinutes() + ':' +today.getSeconds();
+    dataUpdate = Object.fromEntries(Object.entries(dataUpdate).filter(([_,v])=> v != ''))
+    const size = Math.round(dataUpdate['file'].size/1024) + 'KB';
+    dataUpdate['file'] = dataUpdate['file'].name;
+    dataUpdate['size'] = size;
+    dataUpdate['update'] = update;
+    dataUpdate['quality'] = 'High';
+    console.log('1',dataUpdate);
+    updateData('data',dataUpdate);
 }
 function appearScientist(){
     if($('#roleUser').val() == 'DATA SCIENTIST'){
@@ -262,6 +242,17 @@ function appearScientist(){
         $('.data-scientist').css("display","none");
         $('.data-scientist').css("width","50%");
     }
+}
+
+function scientistClean(){
+    var elemToClean;
+    $("input[name='select']").each(function(){
+        if(this.checked){
+            elemToClean = this.value;
+            console.log(elemToClean);
+            window.location.href = "provides_data_clean.html?id="+elemToClean;
+        }
+    })
 }
 function validateEvent(){
     if(event1){
@@ -294,10 +285,10 @@ function adminEdit(){
     window.location.href="edits_user.html"
 }
 
-function loadType() {
+function loadProvideData() {
     var type = ["STRUCTURED","UNSTRUCTURED"];
     for(var i=0;i<type.length;i+=1){
-        var option = document.createElement("OPTION");
+        var option = document.createElement("option");
         option.innerHTML=type[i];
         document.getElementById("typeData").appendChild(option)
 
@@ -305,36 +296,31 @@ function loadType() {
 }
 
 function loadFormat() {
-
     var listFormats = {
       STRUCTURED: ["ATTRIBUTES","DATABASE"],
       UNSTRUCTURED: ["IMAGES","VIDEO FOOTAGE","AUDIO","HANDWRITTEN NOTE"]
-    }
-    
+    }   
     var type = document.getElementById("typeData")
     var format = document.getElementById("formatData")
     var selectedType = type.value;
-    // Se limpian los pueblos
+    // Se limpian los tipos
     format.innerHTML = '<option value="">Select format ...</option>'
     
     if(selectedType !== ''){
-      // Se seleccionan los pueblos y se ordenan
+      // Se seleccionan los tipos y se ordenan
       selectedType = listFormats[selectedType]
     
-      // Insertamos los pueblos
+      // Insertamos los tipos
       for(var i=0;i<selectedType.length;i+=1){
-          var option = document.createElement("OPTION");
+          var option = document.createElement("option");
           option.innerHTML=selectedType[i];
           document.getElementById("formatData").appendChild(option)
 
       }
     }
-    
   }
 
-function scientistClean(){
-    window.location.href="provides_data.html"
-}
+
 
 function extractSummary(){
     window.location.href="extracts_summary_2.html"
